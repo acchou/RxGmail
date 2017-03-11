a8ed7973d8d446306167a0da7153179285fbae3f
Andy Chou <acchou2@gmail.com>
2017-03-10 17:01:02 -0800

    Enable tests on example app.

--- Example/Tests/Tests.swift
+++ Example/Tests/Tests.swift
@@ -1,6 +1,6 @@
 import UIKit
 import XCTest
-import RxGmail
+@testable import RxGmail_Example
 
 class Tests: XCTestCase {
     
@@ -15,15 +15,7 @@ class Tests: XCTestCase {
     }
     
     func testExample() {
-        // This is an example of a functional test case.
-        XCTAssert(true, "Pass")
+        global.rxGmail.listLabels()
+            .map { $0.labels! }
     }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure() {
-            // Put the code you want to measure the time of here.
-        }
-    }
-    
 }
