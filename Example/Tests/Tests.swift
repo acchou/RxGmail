0bd29838f743f685741578c6748f54e119434033
Andy Chou <acchou2@gmail.com>
2017-03-13 23:02:10 -0700

    Add RxBlocking for tests.
    Add a very simple test case that is currently failing.

--- Example/Tests/Tests.swift
+++ Example/Tests/Tests.swift
@@ -1,12 +1,16 @@
 import UIKit
 import XCTest
+import RxSwift
+import RxBlocking
 @testable import RxGmail_Example
 
 class Tests: XCTestCase {
-    
+    var disposeBag: DisposeBag!
+
     override func setUp() {
         super.setUp()
         // Put setup code here. This method is called before the invocation of each test method in the class.
+        disposeBag = DisposeBag()
     }
     
     override func tearDown() {
@@ -15,7 +19,15 @@ class Tests: XCTestCase {
     }
     
     func testExample() {
-        let x = global.rxGmail.listLabels()
-            .map { $0.labels! }
+        let inbox = global.rxGmail.listLabels()
+            .map { $0.labels?
+                .map { $0.name }
+                .filter { $0 == "INBOX" }
+                .first
+            }
+            .unwrap()
+            .unwrap()
+            .toBlocking()
+        XCTAssert(try! inbox.single() == "INBOX", "Could not load label INBOX")
     }
 }
